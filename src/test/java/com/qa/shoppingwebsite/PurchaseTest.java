8e26565d3431ae7b083bb2436fdbf1aca81a44ab
qa-ihussain <ihussain@qa.com>
2021-01-25 17:07:37 +0000

    shopping website tasks

--- src/test/java/com/qa/shoppingwebsite/PurchaseTest.java
+++ src/test/java/com/qa/shoppingwebsite/PurchaseTest.java
@@ -3,6 +3,7 @@ package com.qa.shoppingwebsite;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.AfterAll;
@@ -13,8 +14,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class ShoppingWebsiteTest {
+public class PurchaseTest {
 	private static WebDriver driver;
 	private static WebElement targ;
 
@@ -25,18 +28,38 @@ public class ShoppingWebsiteTest {
 	}
 
 	@Test
-	public void SearchBar() throws InterruptedException {
+	public void Purchase() throws InterruptedException {
 		driver.get("http://automationpractice.com/index.php");
+		driver.findElement(By.className("login")).click();
 
-		// search bar
+		// WAIT for email box to load
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
+
+		driver.findElement(By.id("email")).sendKeys("guest@mail.com");
+		driver.findElement(By.id("passwd")).sendKeys("guest");
+		driver.findElement(By.id("SubmitLogin")).click();
+
 		targ = driver.findElement(By.id("search_query_top"));
 		targ.sendKeys("dress");
 		targ.submit();
 
 		assertEquals("Search - My Store", driver.getTitle());
 
+		List<WebElement> products = driver.findElements(By.className("product-container"));
+		System.out.println(products.size());
+		products.get(0).click(); // Click first product
+
+		// On product page, add to cart
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("add_to_cart")));
+		driver.findElement(By.id("add_to_cart")).click();
+
+		// Proceed To Checkout button
+		driver.findElement(By.xpath("//a[@href='http://automationpractice.com/index.php?controller=order\']")).click();
+
 		// end wait
 		Thread.sleep(5000);
+
 	}
 
 	@AfterAll
