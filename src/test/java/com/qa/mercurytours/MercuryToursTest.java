1358be38722a2f3e71d2323ad6b86c377833b4eb
qa-ihussain <ihussain@qa.com>
2021-01-26 12:19:08 +0000

    task four completed

---
+++ src/test/java/com/qa/mercurytours/MercuryToursTest.java
@@ -0,0 +1,68 @@
+package com.qa.mercurytours;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class MercuryToursTest {
+	private static WebDriver driver;
+
+	@BeforeAll
+	public static void init() {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chrome/chromedriver.exe");
+		driver = new ChromeDriver(chromeCfg());
+	}
+
+	@Test
+	public void register() throws InterruptedException {
+		driver.get("http://demo.guru99.com/test/newtours/");
+		String username = "guest@mail.com";
+		String password = "guest";
+		String confirmPassword = "guest";
+
+		driver.findElement(
+				By.xpath("//html/" + "body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/a"))
+				.click();
+		// creating username and password
+		driver.findElement(By.xpath("//input[@name='email']")).sendKeys(username);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+		driver.findElement(By.xpath("//input[@name='confirmPassword']")).sendKeys(confirmPassword);
+		driver.findElement(By.name("submit")).click();
+
+		// Go to login page
+		driver.findElement(By.xpath("//a[@href='login.php']")).click();
+
+		driver.findElement(By.xpath("//input[@name='userName']")).sendKeys(username);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+		driver.findElement(By.name("submit")).click();
+
+	}
+
+	@AfterAll
+	public static void tearDown() {
+		driver.quit();
+	}
+
+// Designed to return ChromeOptions to configure new ChromeDrivers in Selenium
+	public static ChromeOptions chromeCfg() {
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		ChromeOptions cOptions = new ChromeOptions();
+
+		// Settings
+		prefs.put("profile.default_content_setting_values.cookies", 2);
+		prefs.put("network.cookie.cookieBehavior", 2);
+		prefs.put("profile.block_third_party_cookies", true);
+
+		// Create ChromeOptions to disable Cookies pop-up
+		cOptions.setExperimentalOption("prefs", prefs);
+
+		return cOptions;
+	}
+}
